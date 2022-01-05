180831da6ef4aa2bde01ad3953cf396aaa7c953e
praison <praisonvarghese1@gmail.com>
2022-01-05 15:23:33 +0530

    node js jwt example

---
+++ app.js
@@ -0,0 +1,40 @@
+require('dotenv').config()
+const express=require('express');
+const app = express();
+const jwt = require('jsonwebtoken');
+
+app.use(express.json());
+
+
+const posts =[{
+    username : 'Harsha',
+    tittle : 'Post 1'
+},
+{
+    username : 'Joi',
+    tittle:'Post 2'
+}]
+
+app.get('/posts',authenticateToken,(req,res)=>{
+        res.json(posts.filter(post=>post.username === req.user.name));
+})
+
+app.post('/login',(req,res)=>{
+    const username =req.body.username;
+    const user = {name:username}
+    const accesstoken = jwt.sign(user,process.env.ACCESS_TOKEN_SECRET)
+    res.json({accesstoken: accesstoken})
+})
+
+function authenticateToken(req,res,next){
+    const authHeadeer=req.headers['authorization']
+    const token =authHeadeer && authHeadeer.split(' ')[1]
+    if(token == null) return res.sendStatus(401);
+    jwt.verify(token,process.env.ACCESS_TOKEN_SECRET,(err,user)=>{
+        if(err) return res.sendStatus(403)
+        req.user = user;
+        next()
+    }) 
+}
+
+app.listen(3000);
