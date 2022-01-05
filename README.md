984d88d249f62e7b54b7cb20e0f297e27ed98dca
praison <praisonvarghese1@gmail.com>
2022-01-05 16:22:22 +0530

    adding refresh token logic

--- README.md
+++ README.md
@@ -10,14 +10,15 @@ REQUEST
 
 RESPONSE
 {
-    "accesstoken": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoiSm9pIiwiaWF0IjoxNjQxMzc1ODk5fQ.qFCmxXRSW2g1oftZRyv7xmFUSvkKac5BZ6aSymKRFuc"
+    "accesstoken": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoiSm9pIiwiaWF0IjoxNjQxMzc5NzQ4LCJleHAiOjE2NDEzNzk3NjN9.q494tLnTSzL7wSHxb8b2slKW-8JoqV9Soc8bAo_HBB4",
+    "resfreshtoken": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoiSm9pIiwiaWF0IjoxNjQxMzc5NzQ4fQ.PEV5KhAmJqi7Vi7lFr7SZpEd2uLYDhXgcfNsZpy7IGY"
 }
 
 2. http://localhost:3000/posts
 
 METHOD: GET
 
-HEADER : Authorization : Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoiSm9pIiwiaWF0IjoxNjQxMzc1ODk5fQ.qFCmxXRSW2g1oftZRyv7xmFUSvkKac5BZ6aSymKRFuc
+HEADER : Authorization : Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoiSm9pIiwiaWF0IjoxNjQxMzc5NzQ4LCJleHAiOjE2NDEzNzk3NjN9.q494tLnTSzL7wSHxb8b2slKW-8JoqV9Soc8bAo_HBB4
 
 RESPONSE
 [
@@ -26,4 +27,31 @@ RESPONSE
         "tittle": "Post 2"
     }
 ]
+
+3.http://localhost:3000/refreshtoken 
+
+METHOD: POST
+
+REQUEST 
+{
+    "token":"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoiSm9pIiwiaWF0IjoxNjQxMzc5NzQ4fQ.PEV5KhAmJqi7Vi7lFr7SZpEd2uLYDhXgcfNsZpy7IGY"
+}
+
+RESPONSE
+{
+    "accesstoken": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoiSm9pIiwiaWF0IjoxNjQxMzc5NzgwLCJleHAiOjE2NDEzNzk3OTV9.mEbA3Xpw_62X3XllL3jtG443b4OUA5jedY1Mdb1BJiA"
+}
+
+4.http://localhost:3000/logout
+
+METHOD: DELETE
+
+REQUEST 
+{
+    "token":"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoiSm9pIiwiaWF0IjoxNjQxMzc5NzQ4fQ.PEV5KhAmJqi7Vi7lFr7SZpEd2uLYDhXgcfNsZpy7IGY"
+}
+
+RESPONSE
+OK
+
 
--- app.js
+++ app.js
@@ -15,16 +15,45 @@ const posts =[{
     tittle:'Post 2'
 }]
 
-app.get('/posts',authenticateToken,(req,res)=>{
-        res.json(posts.filter(post=>post.username === req.user.name));
-})
+const refreshTokens=[];
 
 app.post('/login',(req,res)=>{
     const username =req.body.username;
     const user = {name:username}
-    const accesstoken = jwt.sign(user,process.env.ACCESS_TOKEN_SECRET)
-    res.json({accesstoken: accesstoken})
+    const accesstoken =generateAccessToken(user);
+    const refreshToken = jwt.sign(user,process.env.REFRESH_TOKEN_SECRET)
+    refreshTokens.push(refreshToken);
+    res.json({accesstoken: accesstoken,resfreshtoken : refreshToken})
 })
+
+app.post('/refreshToken',(req,res)=>{
+    const refreshToken = req.body.token;
+    if(refreshToken== null) return res.sendStatus(401)
+    if(!refreshTokens.includes(refreshToken)) return res.sendStatus(403)
+    jwt.verify(refreshToken,process.env.REFRESH_TOKEN_SECRET,(err,user)=>{
+        if(err) return res.sendStatus(403)
+        const accesstoken = generateAccessToken({name : user.name});
+        res.json({accesstoken:accesstoken})
+    })
+})
+
+app.get('/posts',authenticateToken,(req,res)=>{
+    res.json(posts.filter(post=>post.username === req.user.name));
+})
+
+app.delete("/logout",(req,res)=>{
+
+   let refreshToken=refreshTokens.find(token=>token === req.body.token)
+   if(refreshToken == null) return res.sendStatus(400)
+   const index = refreshTokens.indexOf(refreshToken);
+   refreshTokens.splice(index,1);
+
+    res.sendStatus(200)
+})
+
+function generateAccessToken(user){
+   return jwt.sign(user,process.env.ACCESS_TOKEN_SECRET,{expiresIn :'15s'})
+}
 
 function authenticateToken(req,res,next){
     const authHeadeer=req.headers['authorization']
